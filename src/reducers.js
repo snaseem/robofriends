dcc452805a7e329db85d166a3ab4713349370ab7
snaseem <snaseem@outlook.com>
2018-12-05 13:49:26 +0500

    Reduxing search field component

--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,14 @@
 import { CHANGE_SEARCH_FIELD } from "./constants.js";
 
 const initialState = {
-    searchField = ''
-}
+  searchField: ""
+};
 
-export const searchRobots = (state=initialState, action={}) => {
-
-    switch(action.type) {
-case CHANGE_SEARCH_FIELD:
-return Object.assign({}, state, {searchField: action.payload})
-    }
-}
+export const searchRobots = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case CHANGE_SEARCH_FIELD:
+      return Object.assign({}, state, { searchField: action.payload });
+    default:
+      return state;
+  }
+};
